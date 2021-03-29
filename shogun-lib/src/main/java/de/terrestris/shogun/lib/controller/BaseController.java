dea16d8f04bca03ca5b183e48538a199c48de871
Andre Henn <henn@terrestris.de>
2021-03-29 17:28:39 +0200

    :lipstick:

--- shogun-lib/src/main/java/de/terrestris/shogun/lib/controller/BaseController.java
+++ shogun-lib/src/main/java/de/terrestris/shogun/lib/controller/BaseController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.OffsetDateTime;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
